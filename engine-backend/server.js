34d5e7d8fbc44e57b3d3d7c95ac859f8d1238cb4
Abhishek Gupta <guptaabhishek60420@gmail.com>
2026-03-23 00:22:46 +0530

    import .env

--- engine-backend/server.js
+++ engine-backend/server.js
@@ -5,13 +5,16 @@ import registerGameSocket from "./src/sockets/game.socket.js"
 
 const server = http.createServer(app)
 
-const io = new Server(server,{
-  cors:{origin:"*"}
-})
-
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_BASE_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 registerGameSocket(io)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- engine-backend/src/app.js
+++ engine-backend/src/app.js
@@ -2,10 +2,17 @@ import express from "express"
 import cors from "cors"
 import gameRoutes from "./routes/game.routes.js"
 import leaderboardRoutes from "./routes/leaderboard.routes.js"
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_BASE_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+}))
 app.use(express.json())
 
 app.use("/api/game",gameRoutes)
