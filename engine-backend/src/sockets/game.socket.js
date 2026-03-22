44a70cae9f74cd469d3fccc7868de70cd1b4650f
Abhishek Gupta <guptaabhishek60420@gmail.com>
2026-03-22 21:59:16 +0530

    updated

--- engine-backend/src/sockets/game.socket.js
+++ engine-backend/src/sockets/game.socket.js
@@ -1,55 +1,55 @@
-import { createGame,getGame,gameExists } from "../services/gameState.service.js"
+import { createGame, getGame, gameExists } from "../services/gameState.service.js"
 import { updateLeaderboard } from "../services/leaderboard.service.js"
 
-export default function registerGameSocket(io){
+export default function registerGameSocket(io) {
 
-  io.on("connection",socket=>{
+  io.on("connection", socket => {
 
-    socket.on("join-game",({roomId,type,playerId})=>{
+    socket.on("join-game", ({ roomId, type, playerId }) => {
 
       socket.join(roomId)
 
-      if(!gameExists(roomId)){
-        createGame(roomId,type)
+      if (!gameExists(roomId)) {
+        createGame(roomId, type)
       }
 
       const game = getGame(roomId)
 
-      if(type==="strands"){
+      if (type === "strands") {
         game.engine.addPlayer(playerId)
       }
 
-      io.to(roomId).emit("game-state",game.engine.getState())
+      io.to(roomId).emit("game-state", game.engine.getState())
 
     })
 
-    socket.on("player-move",({roomId,data})=>{
+    socket.on("player-move", ({ roomId, data }) => {
 
-  const game = getGame(roomId)
+      const game = getGame(roomId)
 
-  if(!game) return
+      if (!game) return
 
-  if(game.type==="dots"){
-    game.engine.handleMove(data)
-  }
+      if (game.type === "dots") {
+        game.engine.handleMove(data)
+      }
 
-  if(game.type==="strands"){
+      if (game.type === "strands") {
 
-    game.engine.submitWord(data.playerId,data.word)
+        game.engine.submitWord(data.playerId, data.word)
 
-    const player = game.engine.state.players[data.playerId]
+        const player = game.engine.state.players[data.playerId]
 
-    updateLeaderboard(
-      game.type,
-      data.playerId,
-      player.score
-    )
+        updateLeaderboard(
+          game.type,
+          data.playerId,
+          player.score
+        )
 
-  }
+      }
 
-  io.to(roomId).emit("game-state",game.engine.getState())
+      io.to(roomId).emit("game-state", game.engine.getState())
 
-})
+    })
 
   })
 
