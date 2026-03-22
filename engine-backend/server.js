a6fb5f157bde0968e7a4b205abfccf75e16e3230
Abhishek Gupta <guptaabhishek60420@gmail.com>
2026-03-22 22:22:16 +0530

    updates

--- engine-backend/server.js
+++ engine-backend/server.js
@@ -1,32 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const { connect } = require("http2");
-const connectDB = require("./src/config/db");
-const authRoutes = require("./src/routes/authRoutes");
+import http from "http"
+import { Server } from "socket.io"
+import app from "./src/app.js"
+import registerGameSocket from "./src/sockets/game.socket.js"
 
-const app = express();
+const server = http.createServer(app)
 
-//middleware joki cors ko handle karega
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET","POST","PUT","DELETE"],
-        allowedHeaders: ["Content-Type","Authorization"]
-    })
-);
+const io = new Server(server,{
+  cors:{origin:"*"}
+})
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+registerGameSocket(io)
 
-connectDB();
-
-app.use("/api/v1/auth",authRoutes);
-
-//serve uploads folder
-app.use("/uploads",express.static(path.join(__dirname,"uploads")));
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+server.listen(5000,()=>{
+  console.log("Server running on port 5000")
+})
